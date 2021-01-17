f640fb89bd3ac5f26ad3d31665c46df76444410b
Alexis Trudeau <atrudeau@hopper.com>
2021-01-16 21:07:14 -0500

    Ch4 polish

--- src/main/scala/Either.scala
+++ src/main/scala/Either.scala
@@ -3,23 +3,25 @@ package sfpbook
 sealed trait Either[+L, +R] {
   def map[A](f: R => A): Either[L, A]
   def flatMap[LL >: L, A](f: R => Either[LL, A]): Either[LL, A]
+  def getOrElse[A >: R](default: => A): A
   def orElse[LL >: L, A >: R](default: => Either[LL, A]): Either[LL, A]
   def map2[LL >: L, A, B](a: Either[LL, A])(f: (R, A) => B): Either[LL, B]
 }
 case class Left[+L](left: L) extends Either[L, Nothing] {
   override def map[A](f: Nothing => A) = this
   override def flatMap[LL >: L, A](f: Nothing => Either[LL, A]) = this
+  override def getOrElse[A](default: => A): A = default
   override def orElse[LL >: L, A](default: => Either[LL, A]) = default
   override def map2[LL >: L, A, B](a: Either[LL, A])(f: (Nothing, A) => B) = this
+
 }
 
 case class Right[+R](right: R) extends Either[Nothing, R] {
   override def map[A](f: R => A): Either[Nothing, A] = Right(f(right))
   override def flatMap[Nothing, A](f: R => Either[Nothing, A]) = f(right)
+  override def getOrElse[A >: R](default: => A): A = right
   override def orElse[Nothing, A >: R](default: => Either[Nothing, A]) = this
-  override def map2[LL >: Nothing, A, B](a: Either[LL, A])(f: (R, A) => B) = a.map { r2 =>
-    f(right, r2)
-  }
+  override def map2[LL >: Nothing, A, B](a: Either[LL, A])(f: (R, A) => B) = a.map { r2 => f(right, r2) }
 }
 
 object Either {
--- src/test/scala/Ch4Spec.scala
+++ src/test/scala/Ch4Spec.scala
@@ -10,6 +10,13 @@ class Ch4Spec extends FlatSpec with Matchers {
     an[RuntimeException] should be thrownBy None.get
     Some(4).map(_ + 2) shouldEqual Some(6)
     None.map((i:Int) => i + 2) shouldEqual None
+    None.flatMap((i:Int) => Some(i +2)) shouldEqual None
+    Some(4).flatMap(i => Some(i + 2)) shouldEqual Some(6)
+    Some(4).flatMap(i => None) shouldEqual None
+    None.filter(_ => true) shouldEqual None
+    Some(5).filter(_ => false) shouldEqual None
+    Some(5).filter(_ == 5) shouldEqual Some(5)
+    Some(5).filter(_ == 4) shouldEqual None
   }
   "Ch4.5" should "traverse correctly" in {
     Option.traverse(List(1, 2, 3))(i => Some(i + 1)) shouldEqual Some(List(2, 3, 4))
