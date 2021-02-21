f550fb12afe48c36e466a8a61ae714e680e81568
Alexis Trudeau <atrudeau@hopper.com>
2021-02-21 17:47:34 -0500

    Parser WIP

--- src/main/scala/Parsers.scala
+++ src/main/scala/Parsers.scala
@@ -13,15 +13,26 @@ package sfpbook.ch9
 
 trait Parsers[ParseError, Parser[+_]] { self =>
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
-  def or[A](p1: Parser[A], p2: Parser[A]): Parser[A]
+
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+  val identity: Parser[String]
+  def alwaysFail[A]: Parser[A]
+  def zeroOrMore(a: Char): Parser[Int]
+  def oneOrMore(a: Char): Parser[Int]
+
   implicit def char(a: Char): Parser[Char]
   implicit def string(s: String): Parser[String]
-  implicit def asStringParser[A](a : A)(implicit f: A => Parser[String]): ParserOps[String] =
-    ParserOps(f(a))
 
-  case class ParserOps[A](p: Parser[A]) {
-    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+  protected[this] def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+  protected[this] def and[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+  protected[this] def concat[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+
+  implicit class ParserOps[A](p: Parser[A]) {
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
-
+    def |[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
+    def and[B >: A](p2: => Parser[B]): Parser[B] = self.and(p, p2)
+    def &[B >: A](p2: => Parser[B]): Parser[B] = self.and(p, p2)
+    def +[B >: A](p2: => Parser[B]): Parser[B] = self.concat(p, p2)
+    def concat[B >: A](p2: => Parser[B]): Parser[B] = self.concat(p, p2)
   }
 }
--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -3,27 +3,28 @@ package sfpbook.ch9
 import org.scalacheck.Prop.propBoolean
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.matchers.should.Matchers.exist.and
 import org.scalatest.matchers.should._
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 class ParserSpec extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks {
   type Parser[A] = Int
   val p: Parsers[String, Parser] = ???
-  "Parsers" should ".char parses simple character" in {
+  "Parsers" should "parses simple character" in {
     forAll { (c1: Char, c2: Char) => whenever(c1 != c2) {
       import p._
       run(c1)(c1.toString) shouldEqual Right(c1)
       run(c1)(c2.toString) shouldBe a [Left[_, _]]
     }}
   }
-  it should ".string parses simple string" in {
+  it should "parses simple string" in {
     forAll { (s1: String, s2: String) => whenever(s1 != s2) {
       import p._
       run(s1)(s1) shouldEqual Right(s1)
       run(s1)(s2) shouldBe a[Left[_, _]]
     }}
   }
-  it should ".or parses either string" in {
+  it should "| operator parses either string" in {
     forAll { (s1: String, s2: String, s3: String) => whenever(s3 != s1 && s3 != s2) {
       import p._
       run(s1 | s2)(s1) shouldEqual Right(s1)
@@ -31,4 +32,23 @@ class ParserSpec extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks
       run(s1 | s2)(s3) shouldBe a[Left[_, _]]
     }}
   }
+  it should "listOfN parses multiple of" in {
+    forAll { (s1: String, s2: String) =>
+      import p._
+      val pattern = listOfN(2, s1 | s2)
+      for { i <- s1 :: s2 :: Nil; j <- s1 :: s2 :: Nil } yield {
+        run(pattern)(i + j) shouldEqual Right(i + j)
+      }
+    }
+  }
+  // `or` Laws
+  // reflective: x | x === x
+  // commutative: x | y === y | x
+  // associative: x | (y | z) === (x | y) | z
+  //              alwaysFail | x === x
+  //              identity | x === identity
+
+  // `listOfN` Laws
+  // listOfN(1, x) === x
+  // listOfN(a, listOfN(b, x)) === listOfN(a * b, x)
 }
