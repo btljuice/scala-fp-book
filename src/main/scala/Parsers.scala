4fdccd6d493a32da0369eb1476dc24e143e3b692
Alexis Trudeau <atrudeau@hopper.com>
2021-02-23 19:51:12 -0500

    Parser WIP

--- src/main/scala/Parsers.scala
+++ src/main/scala/Parsers.scala
@@ -1,38 +1,58 @@
 package sfpbook.ch9
 
-// Goal design a Parser combinator through "algebraic design".
-// "Algebraic design" means thinking about the laws that should hold before implementation
-
+// - Goal design a Parser combinator through "algebraic design".
+// - "Algebraic design" means thinking about the laws that should hold before implementation
 // - String as input
 // - What's the input format? (HTML, JSON, [a-z])
 
-
-
-
-// Parser[+_] means the `Parser` is a type constructor
-
+// Parser[+_] means the `Parser` is a type constructor.
+// ANSME: I'm not sure yet how the covariant. Does it mean Parser implementation has
+//        to be covariant on its parameter. We'll figure it out pretty soon.
 trait Parsers[ParseError, Parser[+_]] { self =>
+  // Executes the parser
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
-  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
-  val identity: Parser[String]
+  // Some default parsers. Not sure yet if they are all relevant
+  val noop: Parser[String] // Should return the string unparsed
   def alwaysFail[A]: Parser[A]
-  def zeroOrMore(a: Char): Parser[Int]
-  def oneOrMore(a: Char): Parser[Int]
+  final def empty: Parser[Boolean] = noop.map(_.isEmpty)
+  def nonEmpty: Parser[Boolean] = noop.map(_.nonEmpty)
+  final def succeed[A](a: => A): Parser[A] = noop.map(_ => a)
 
-  implicit def char(a: Char): Parser[Char]
+  final def count0(a: Char): Parser[Int] = char(a).many.map(_.size)
+  final def count1(a: Char): Parser[Int] = char(a).many1.map(_.size)
+
+  implicit def char(a: Char): Parser[Char] = string(a.toString).map(_.head)
   implicit def string(s: String): Parser[String]
 
-  protected[this] def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
-  protected[this] def and[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
-  protected[this] def concat[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+  // Primitives to implement
+  // - "and" operator: For an "and" parser, we'd have to decide for which of the
+  //   2 successful runs, we retain the result. An arbitrary choice could be made. (1st one)
 
+  // Let's say we decide to retain the first result, then this operation is not commutative
+  // The operation will be associative
+  protected[this] def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
+  protected[this] def map[A, B](p: Parser[A])(f: A => B): Parser[B]
+  protected[this] def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+  protected[this] def many[A](p: Parser[A], from: Int, to: Option[Int]): Parser[List[A]]
+  protected[this] def concatenate[A](ps: Seq[Parser[A]]): Parser[List[A]]
+  protected[this] def join[A, B](pa: Parser[A], pb: Parser[B]): Parser[(A, B)]
+
+  // Operators for parser's expressiveness
   implicit class ParserOps[A](p: Parser[A]) {
     def or[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
-    def |[B >: A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
-    def and[B >: A](p2: => Parser[B]): Parser[B] = self.and(p, p2)
-    def &[B >: A](p2: => Parser[B]): Parser[B] = self.and(p, p2)
-    def +[B >: A](p2: => Parser[B]): Parser[B] = self.concat(p, p2)
-    def concat[B >: A](p2: => Parser[B]): Parser[B] = self.concat(p, p2)
+    def |[B >: A](p2: => Parser[B]): Parser[B] = or(p2)
+
+    def map[B](f: A => B): Parser[B] = self.map(p)(f)
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
+
+    def many: Parser[List[A]] = many(0)
+    def many(from: Int, to: Int): Parser[List[A]] = many(from, Some(to))
+    def many(from: Int, to: Option[Int] = None): Parser[List[A]] = self.many(p, from, to)
+    def many1: Parser[List[A]] = many(1)
+    def many1(to: Int): Parser[List[A]] = many(1, to)
+    def many1(to: Option[Int]): Parser[List[A]] = many(1, to)
+
+    def exactly(n: Int): Parser[List[A]] = many(n, n)
   }
 }
--- src/test/scala/ParserSpec.scala
+++ src/test/scala/ParserSpec.scala
@@ -1,5 +1,7 @@
 package sfpbook.ch9
 
+import org.scalacheck.Gen
+import org.scalacheck.Prop
 import org.scalacheck.Prop.propBoolean
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
@@ -9,7 +11,7 @@ import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
 class ParserSpec extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks {
   type Parser[A] = Int
-  val p: Parsers[String, Parser] = ???
+  lazy val p: Parsers[String, Parser] = ???
   "Parsers" should "parses simple character" in {
     forAll { (c1: Char, c2: Char) => whenever(c1 != c2) {
       import p._
@@ -32,23 +34,65 @@ class ParserSpec extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks
       run(s1 | s2)(s3) shouldBe a[Left[_, _]]
     }}
   }
-  it should "listOfN parses multiple of" in {
+  it should "exactly parses multiple of" in {
     forAll { (s1: String, s2: String) =>
       import p._
-      val pattern = listOfN(2, s1 | s2)
+      val pattern = (s1 | s2).exactly(2)
       for { i <- s1 :: s2 :: Nil; j <- s1 :: s2 :: Nil } yield {
         run(pattern)(i + j) shouldEqual Right(i + j)
       }
     }
   }
-  // `or` Laws
-  // reflective: x | x === x
-  // commutative: x | y === y | x
-  // associative: x | (y | z) === (x | y) | z
-  //              alwaysFail | x === x
-  //              identity | x === identity
+  it should "succeed always return true" in {
+    forAll { (s: String) =>
+      import p._
+      run(succeed(true))(s) shouldEqual Right(true)
+    } }
+  it should "alwaysFail always return error" in {
+    forAll { (s: String) =>
+      import p._
+      run(alwaysFail[Unit])(s) shouldBe a[Left[_, _]]
+    } }
+  it should "empty and nonEmpty query the string" in {
+    p.run(p.empty)("") shouldEqual true
+    p.run(p.empty)("a") shouldEqual false
+    p.run(p.nonEmpty)("") shouldEqual false
+    p.run(p.nonEmpty)("a") shouldEqual true
+  }
+  it should "noop returns the string unparsed" in {
+    forAll { (s: String) =>
+    import p._
+    run(noop)(s) shouldEqual s
+  } }
 
-  // `listOfN` Laws
-  // listOfN(1, x) === x
-  // listOfN(a, listOfN(b, x)) === listOfN(a * b, x)
+  object Laws {
+    import p._
+    def ===[A](p1: Parser[A], p2: Parser[A])(implicit g: Gen[String]) = forAll(g){ s => run(p1)(s) == run(p2)(s) }
+
+    def mapLaw[A](p1: Parser[A])(implicit g: Gen[String]) =
+      p1 === p1.map((a: A) => a)
+
+    def flatMapLaw[A](p1: Parser[A])(implicit g: Gen[String]) =
+      p1 === p1.flatMap[A](_ => p1)
+
+    def exactly1Law[A](p1: Parser[A])(implicit g: Gen[String]) =
+      p1 === p1.exactly(1)
+
+    def exactlyDistributionLaw[A](p1: Parser[A])(implicit g: Gen[String]) =
+      forAll { (a: Int, b: Int) => whenever(a >= 0 && b >= 0) {
+        p1.exactly(a * b) === p1.exactly(a).exactly(b)
+      } }
+
+    def manyDistributionLaw[A](p1: Parser[A])(implicit g: Gen[String]) =
+      forAll { (a: Int, b: Int, c: Int, d: Int) =>
+        whenever(0 <= a && a <= b && 0 <= c && c <= d) {
+          p1.many(a, b).many(c, d) === p1.many(a*c, b*d)
+        } }
+
+    def orReflectiveLaw[A](p1: Parser[A])(implicit g: Gen[String]) =
+      p1 === (p1 | p1)
+    def orAssociativeLaw[A](p1: Parser[A], p2: Parser[A], p3: Parser[A])(implicit g: Gen[String]) =
+      (p1 | (p2 | p3)) === ((p1 | p2) | p3)
+
+  }
 }
