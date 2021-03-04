16a8195cafff623043e0924c377da65bff9a2e42
Alexis Trudeau <atrudeau@hopper.com>
2021-03-03 21:16:29 -0500

    Parsers: Location as input

--- src/main/scala/SimpleParser.scala
+++ src/main/scala/SimpleParser.scala
@@ -2,15 +2,15 @@ package sfpbook.ch9
 
 import scala.util.matching.Regex
 
-case class SimpleParser[+A](run: String => SimpleParser.Result[A], attempt: Boolean = false)
+case class SimpleParser[+A](run: Location => SimpleParser.Result[A], attempt: Boolean = false)
 
 object SimpleParser {
   sealed trait Result[+A] {
     def map[B >: A](f: A => B): Result[B]
     def mapError(f: ParseError => ParseError): Result[A]
  }
-  case class ParseSuccess[+A](get: A, location: Location) extends Result[A] {
-    override def map[B >: A](f: A => B): Result[B] = ParseSuccess(f(get), location)
+  case class ParseSuccess[+A](get: A, charsConsumed: Int) extends Result[A] {
+    override def map[B >: A](f: A => B): Result[B] = ParseSuccess(f(get), charsConsumed)
     override def mapError(f: ParseError => ParseError): Result[A] = this
   }
   case class ParseError(stack: List[(String, Location)]) extends Result[Nothing] {
@@ -21,77 +21,73 @@ object SimpleParser {
     def lastLocation: Location = last._2
     def push(msg: String, l: Location = lastLocation): ParseError = ParseError((msg, l) :: stack)
   }
-  object ParseSuccess {
-    def apply[A](get: A, input: String, offset: Int = 0): ParseSuccess[A] = ParseSuccess(get, Location(input, offset))
-  }
   object ParseError {
-    def apply(msg: String, input: String, offset: Int = 0): ParseError = ParseError(msg, Location(input, offset))
-    def apply(msg: String, l: Location): ParseError = ParseError((msg, l))
-    def apply(entries: (String, Location)*): ParseError = ParseError(entries.toList)
+    def apply(msg: String, input: String, offset: Int): ParseError = ParseError(msg, Location(input, offset))
+    def apply(msg: String, l: Location): ParseError = ParseError((msg, l) :: Nil)
   }
 }
 
 object SimpleParsers extends Parsers[SimpleParser.ParseError, SimpleParser] {
   import SimpleParser._
 
-  private implicit def sp[A](f: String => Result[A]): SimpleParser[A] = SimpleParser(f)
+  private implicit def sp[A](f: Location => Result[A]): SimpleParser[A] = SimpleParser(f)
 
-  override def run[A](pa: SimpleParser[A])(input: String): Either[ParseError, A] = pa.run(input) match {
+  override def run[A](pa: SimpleParser[A])(input: String): Either[ParseError, A] = pa.run(Location(input, 0)) match {
     case ParseSuccess(a, _) => Right(a)
     case e: ParseError => Left(e)
   }
 
-  override val noop = sp { i => ParseSuccess(i, i, i.length) }
+  override val noop = sp { l => ParseSuccess(l.after, l.after.length) }
 
   override def delay[A](pa: => SimpleParser[A]) = sp { pa.run }
   override def attempt[A](pa: SimpleParser[A]) = SimpleParser(pa.run, attempt = true)
 
   override def alwaysFail[A]= sp { ParseError("alwaysFail", _) }
-  override def succeed[A](a: => A) = sp { ParseSuccess(a, _) }
+  override def succeed[A](a: => A) = sp { _ => ParseSuccess(a, 0) }
 
-  override def string(s: String) = sp { i =>
-    def diff: Int = s zip i indexWhere { case (x, y) => x != y }
-    if (i.startsWith(s)) ParseSuccess(s, i, s.length)
-    else ParseError(s"string mismatch ($s)", i, diff)
+  override def string(s: String) = sp { l =>
+    def diff: Int = s zip l.after indexWhere { case (x, y) => x != y }
+    if (l.after.startsWith(s)) ParseSuccess(s, s.length)
+    else ParseError(s"string mismatch ($s)", l + diff)
   }
 
-  override implicit def regex(r: Regex) = sp { i =>
-    r.findPrefixMatchOf(i)
-      .map { m => ParseSuccess(m.matched, i, m.end) }
-      .getOrElse { ParseError(s"regex mismatch $r", i) }
+  override implicit def regex(r: Regex) = sp { l =>
+    r.findPrefixMatchOf(l.after)
+      .map { m => ParseSuccess(m.matched, m.end) }
+      .getOrElse { ParseError(s"regex mismatch $r", l) }
   }
 
-  override protected[this] def or[A](p1: SimpleParser[A], p2: => SimpleParser[A]) = sp { i =>
-    p1.run(i) match {
+  override protected[this] def or[A](p1: SimpleParser[A], p2: => SimpleParser[A]) = sp { l =>
+    p1.run(l) match {
       case s: ParseSuccess[A] => s
-      case _: ParseError if p1.attempt => p2.run(i)
+      case _: ParseError if p1.attempt => p2.run(l)
       case e: ParseError => e
     }
   }
 
-  override protected[this] def flatMap[A, B](p: SimpleParser[A])(f: A => SimpleParser[B]): SimpleParser[B] = sp { i =>
-    p.run(i) match {
-      case ParseSuccess(get, location) => f(get).run(location.after)
+  override protected[this] def flatMap[A, B](p: SimpleParser[A])(f: A => SimpleParser[B]): SimpleParser[B] = sp { l =>
+    p.run(l) match {
+      case ParseSuccess(get, n) => f(get).run(l + n)
       case e: ParseError => e
     }
   }
 
-  override protected[this] def slice[A](pa: SimpleParser[A]): SimpleParser[String] = sp { i =>
-    pa.run(i) match {
-      case ParseSuccess(_, location) => ParseSuccess(location.before, location)
+  override protected[this] def slice[A](pa: SimpleParser[A]): SimpleParser[String] = sp { l =>
+    pa.run(l) match {
+      case ParseSuccess(_, n) => ParseSuccess(l.after.substring(0, n), n)
       case e: ParseError => e
     }
   }
 
-  override def label[A](msg: String)(p: SimpleParser[A]): SimpleParser[A] = sp { i =>
-    p.run(i).mapError {
-      case ParseError(h :: t) => ParseError((msg, h._2) :: t)
+  override def label[A](msg: String)(p: SimpleParser[A]): SimpleParser[A] = sp { l =>
+    p.run(l).mapError {
+      case ParseError((_, loc) :: t) => ParseError((msg, loc) :: t)
       case ParseError(Nil) => sys.error("Unexpected. Should be a non empty-list")
     }
   }
 
-  override def scope[A](msg: String)(p: SimpleParser[A]): SimpleParser[A] = sp { i =>
-    p.run(i).mapError { _.push(msg) }
+  override def scope[A](msg: String)(p: SimpleParser[A]): SimpleParser[A] = sp { l =>
+    p.run(l).mapError { _.push(msg) }
   }
 
   override def errorLocation(e: ParseError): Location = e.stack.head._2
