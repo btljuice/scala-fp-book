9c1598fd020b95add4fc196c4d5ed77daab2e5eb
Alexis Trudeau <atrudeau@hopper.com>
2021-03-04 22:51:00 -0500

    Ch 10 : Monoids foldMap

--- src/main/scala/Monoid.scala
+++ src/main/scala/Monoid.scala
@@ -6,33 +6,55 @@ trait Monoid[A] {
 }
 
 object Monoids {
-  val stringConcat = new Monoid[String] {
-    def op(a1: String, a2: String) = a1 + a2
-    def zero = ""
+  object Instances {
+    val stringConcat = new Monoid[String] {
+      def op(a1: String, a2: String) = a1 + a2
+
+      def zero = ""
+    }
+
+    def listConcat[A] = new Monoid[List[A]] {
+      def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+
+      val zero = Nil
+    }
+
+    val intAddition = new Monoid[Int] {
+      def op(a1: Int, a2: Int): Int = a1 + a2
+
+      val zero = 0
+    }
+
+    val intMultiplication = new Monoid[Int] {
+      def op(a1: Int, a2: Int) = a1 * a2
+
+      val zero = 1
+    }
+
+    val booleanOr = new Monoid[Boolean] {
+      def op(a1: Boolean, a2: Boolean) = a1 || a2
+
+      val zero = false
+    }
+
+    val booleanAnd = new Monoid[Boolean] {
+      def op(a1: Boolean, a2: Boolean) = a1 && a2
+
+      val zero = true
+    }
+
+    def optionOrElse[A] = new Monoid[Option[A]] {
+      def op(a1: Option[A], a2: Option[A]) = a1 orElse a2
+
+      val zero = None
+    }
+
+    def composeMonoid[A] = new Monoid[A => A] {
+      def op(f1: A => A, f2: A => A) = a => f1(f2(a))
+      val zero = identity
+    }
   }
 
-  def listConcat[A] = new Monoid[List[A]] {
-    def op(a1: List[A], a2: List[A]): List[A] = a1 ++ a2
-    val zero = Nil
-  }
-
-  val intAddition = new Monoid[Int] {
-    def op(a1: Int, a2: Int): Int = a1 + a2
-    val zero = 0
-  }
-
-  val intMultiplication = new Monoid[Int] {
-    def op(a1: Int, a2: Int) = a1 * a2
-    val zero = 1
-  }
-
-  val booleanOr = new Monoid[Boolean] {
-    def op(a1: Boolean, a2: Boolean) = a1 || a2
-    def zero = false
-  }
-
-  val booleanAnd = new Monoid[Boolean] {
-    def op(a1: Boolean, a2: Boolean) = a1 && a2
-    def zero = true
-  }
+  def foldMap[A, B](as: List[A])(f: A => B)(implicit m: Monoid[B]): B =
+    as.foldLeft(m.zero){ (b, a) => m.op(b, f(a)) }
 }
