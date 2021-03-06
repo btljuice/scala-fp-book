379af0e6cfdd093b79134611cea191481399d3bc
Alexis Trudeau <atrudeau@hopper.com>
2021-03-06 18:35:19 -0500

    Ch 10 - Monoid Law Property tests

--- src/main/scala/Monoid.scala
+++ src/main/scala/Monoid.scala
@@ -1,5 +1,8 @@
 package sfpbook.ch10
 
+/** Requirements:
+ * 1. op must be associative === op(a, op(b, c)) == op(op(a, b), c)
+ * 2. op(zero, x) == x && op(x, zero) == x */
 trait Monoid[A] {
   def op(a1: A, a2: A): A
   def zero: A
@@ -39,10 +42,10 @@ object Monoid {
   }
 
   def foldRight[A, B](as: List[A])(z: B)(f: (A, B) => B): B =
-    foldMap(as)(a => f(a, _))(Instances.composeMonoid[B])(z)
+    foldMap(as)(a => f(a, _))(Instances.compose[B])(z)
 
   def foldLeft[A, B](as: List[A])(z: B)(f: (B, A) => B): B =
-    foldMap(as)(a => f(_, a))(Instances.andThenMonoid[B])(z)
+    foldMap(as)(a => f(_, a))(Instances.andThen[B])(z)
 
   object Instances {
     val stringConcat = new Monoid[String] {
@@ -73,11 +76,11 @@ object Monoid {
       def op(a1: Option[A], a2: Option[A]) = a1 orElse a2
       val zero = None
     }
-    def composeMonoid[A] = new Monoid[A => A] {
+    def compose[A] = new Monoid[A => A] {
       def op(f1: A => A, f2: A => A) = a => f1(f2(a))
       val zero = identity
     }
-    def andThenMonoid[A] = new Monoid[A => A] {
+    def andThen[A] = new Monoid[A => A] {
       def op(f1: A => A, f2: A => A) = a => f2(f1(a))
       val zero = identity
     }
--- src/test/scala/MonoidSpec.scala
+++ src/test/scala/MonoidSpec.scala
@@ -1,5 +1,6 @@
 package sfpbook.ch10
 
+import org.scalacheck.Arbitrary
 import org.scalacheck.Gen
 import org.scalacheck.Prop
 import org.scalacheck.Prop.propBoolean
@@ -21,5 +22,49 @@ class MonoidSpec extends AnyFlatSpec with Matchers with ScalaCheckPropertyChecks
     implicit val m = Monoid.mergeMapMonoid[String, Int](Monoid.Instances.intAddition)
     val actual = Monoid.foldMap(elements)(k => Map(k -> 1))
     actual shouldEqual elements.groupBy(identity).mapValues(_.size)
+  }
+
+  MonoidLaw.test("stringConcat", Monoid.Instances.stringConcat)
+  MonoidLaw.test("listConcat", Monoid.Instances.listConcat[Int]) // ANSME: How to test over arbitrary types?
+  MonoidLaw.test("intAddition", Monoid.Instances.intAddition)
+  MonoidLaw.test("intMultiplication", Monoid.Instances.intMultiplication)
+  MonoidLaw.test("booleanOr", Monoid.Instances.booleanOr)
+  MonoidLaw.test("booleanAnd", Monoid.Instances.booleanAnd)
+  MonoidLaw.test("optionOrElse", Monoid.Instances.optionOrElse[Int])
+  MonoidLaw.testFnc[Int]("compose", Monoid.Instances.compose[Int])
+  MonoidLaw.testFnc[Int]("andThen", Monoid.Instances.andThen[Int])
+
+
+  object MonoidLaw {
+    def test[A](label: String, m: Monoid[A])(implicit arb: Arbitrary[A]) = {
+       label + " monoid instance" should "be associative" in {
+         forAll { (a: A, b: A, c: A) =>
+           m.op(a, m.op(b, c)) shouldEqual m.op(m.op(a, b), c)
+         }
+       }
+       it should "zero act as identity" in {
+         forAll { a: A =>
+           m.op(m.zero, a) shouldEqual a
+           m.op(a, m.zero) shouldEqual a
+         }
+       }
+    }
+
+    def testFnc[A](label: String, m: Monoid[A => A])(implicit arb: Arbitrary[A], arbf: Arbitrary[A => A]) = {
+      label + " monoid instance" should "be associative" in {
+        forAll { (f: A => A, g: A => A, h: A => A) =>
+          val l = m.op(f, m.op(g, h))
+          val r = m.op(m.op(f, g), h)
+          forAll { a: A => l(a) shouldEqual r(a) }
+        }
+      }
+      it should "zero act as identity" in {
+        forAll { (f: A => A) =>
+          val l = m.op(f, m.zero)
+          val r = m.op(m.zero, f)
+          forAll { a: A =>l(a) shouldEqual f(a); r(a) shouldEqual f(a) }
+        }
+      }
+    }
   }
 }
