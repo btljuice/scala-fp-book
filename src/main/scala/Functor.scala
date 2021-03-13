4a8745bbbe728ef851c8a7066f0de3244327bf9f
Alexis Trudeau <atrudeau@hopper.com>
2021-03-13 12:02:37 -0500

    FunctorOps

--- src/main/scala/Functor.scala
+++ src/main/scala/Functor.scala
@@ -2,13 +2,14 @@ package sfpbook.ch11
 
 /** Requirements:
  * 1. map(x)(identity) == x */
-trait Functor[F[_]] {
+trait Functor[F[_]] { self =>
   def map[A, B](fa: F[A])(f: A => B): F[B]
 
   final def distribute[A, B](f: F[(A, B)]): (F[A], F[B]) = ( map(f)(_._1) , map(f)(_._2) )
-  final def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e match {
-    case Left(fa) => map(fa)(Left(_))
-    case Right(fb) => map(fb)(Right(_))
+  final def codistribute[A, B](e: Either[F[A], F[B]]): F[Either[A, B]] = e.fold(map(_)(Left(_)), map(_)(Right(_)))
+
+  implicit class FunctorOps[A](fa: F[A]) {
+    final def map[B](f: A => B): F[B] = self.map(fa)(f)
   }
 }
 
