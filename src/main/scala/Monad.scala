8c0f4e2e1d94ff94c90354ed47b750191ea9ad11
Alexis Trudeau <atrudeau@hopper.com>
2021-03-13 12:38:37 -0500

    Modified Monad to allow 2 set of primitives

--- src/main/scala/Monad.scala
+++ src/main/scala/Monad.scala
@@ -1,30 +1,32 @@
 package sfpbook.ch11
 
+import sfpbook.ch12.Applicative
 import sfpbook.ch6.State
 import sfpbook.ch7.Par
 import sfpbook.ch7.Par.Par
 import sfpbook.ch8.Test.Gen
 
-/** All monads are functor because they implement map (a abide to the same laws) */
-trait Monad[F[_]] extends Functor[F] { self =>
-  def unit[A](a: => A): F[A]
-  def flatMap[A, B](m: F[A])(f: A => F[B]): F[B]
+/** - All monads are functor because they implement map (a abide to the same laws)
+ *  - All monads are applicative because they can implement map2
+ *  Possible primitives:
+ *  1. unit, flatMap
+ *  2. unit, compose
+ *  3, unit, map, join *
+ *  * Contrary to Applicative, trying to be flexible over the primitive would fail for one of the option.
+ *  In the definition below, if only unit, map and join are implemented, compose and flatMap are still implemented
+ *  recursively over themselves.
+ *  Again, the best choice here would either to provide an interface to well specify the chosen primitives OR
+ *  simply allow only one set of primitive.
+ */
+trait Monad[F[_]] extends Applicative[F] { self =>
+  override def unit[A](a: => A): F[A]
+  def flatMap[A, B](m: F[A])(f: A => F[B]): F[B] = compose[Unit, A, B](_ => m, f)(())
+  def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] = a => flatMap(f(a))(g)
 
-  final def map[A, B](m: F[A])(f: A => B): F[B] = flatMap(m)(a => unit(f(a)))
-  final def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] = flatMap(ma) { a => map(mb) { b => f(a, b) } }
+  final override def map[A, B](m: F[A])(f: A => B): F[B] = flatMap(m)(a => unit(f(a)))
+  final override def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] = flatMap(ma) { a => map(mb) { b => f(a, b) } }
 
   final def join[A](mma: F[F[A]]): F[A] = flatMap(mma)(identity)
-  final def sequence[A](l: List[F[A]]): F[List[A]] = traverse(l)(identity)
-  final def traverse[A,B](l: List[A])(f : A => F[B]): F[List[B]] =
-    l.foldRight(unit(List.empty[B])) { (a, acc) => map2(f(a), acc) { _ :: _ } }
-
-  final def replicateM[A](n: Int, m: F[A]): F[List[A]] =
-    (1 to n).foldLeft(unit(List.empty[A])) { (acc, _) => map2(m, acc) { _ :: _ } }
-
-  final def product[A, B](ma: F[A], mb: F[B]): F[(A, B)] = map2(ma, mb){ (_, _) }
-  final def filterM[A](l: List[A])(f: A => F[Boolean]): F[List[A]] =
-    l.foldRight(unit(List.empty[A])) { (a, acc) => map2(f(a), acc) { (p, l) => if(p) a :: l else l } }
-  final def compose[A, B, C](f: A => F[B], g: B => F[C]): A => F[C] = a => flatMap(f(a))(g)
 
   ///// Primitives alternatives:
   // 1. unit, compose
@@ -46,24 +48,24 @@ trait Monad[F[_]] extends Functor[F] { self =>
 object Monad {
   object Instances {
     val genMonad = new Monad[Gen] {
-      def unit[A](a: => A) = Gen.unit(a)
-      def flatMap[A, B](m: Gen[A])(f: A => Gen[B]) = m flatMap f
+      override def unit[A](a: => A) = Gen.unit(a)
+      override def flatMap[A, B](m: Gen[A])(f: A => Gen[B]) = m flatMap f
     }
     val parMonad = new Monad[Par] {
-      def unit[A](a: => A) = Par.unit(a)
-      def flatMap[A, B](m: Par[A])(f: A => Par[B]) = m flatMap f
+      override def unit[A](a: => A) = Par.unit(a)
+      override def flatMap[A, B](m: Par[A])(f: A => Par[B]) = m flatMap f
     }
     val optionMonad = new Monad[Option] {
-      def unit[A](a: => A) = Some(a)
-      def flatMap[A, B](m: Option[A])(f: A => Option[B]) = m flatMap f
+      override def unit[A](a: => A) = Some(a)
+      override def flatMap[A, B](m: Option[A])(f: A => Option[B]) = m flatMap f
     }
     val streamMonad = new Monad[Stream] {
-      def unit[A](a: => A) = Stream(a) // Not lazy over a
-      def flatMap[A, B](m: Stream[A])(f: A => Stream[B]): Stream[B] = m flatMap f
+      override def unit[A](a: => A) = Stream(a) // Not lazy over a
+      override def flatMap[A, B](m: Stream[A])(f: A => Stream[B]): Stream[B] = m flatMap f
     }
     val listMonad = new Monad[List] {
-      def unit[A](a: => A) = a :: Nil
-      def flatMap[A, B](m: List[A])(f: A => List[B]) =  m flatMap f
+      override def unit[A](a: => A) = a :: Nil
+      override def flatMap[A, B](m: List[A])(f: A => List[B]) =  m flatMap f
     }
 
     def stateMonad[T] = {
@@ -76,8 +78,8 @@ object Monad {
     // This is probably the simplest monad that can be. It's just a container over value of A
     case class Id[A](value: A)
     val idMonad = new Monad[Id] {
-      def unit[A](a: => A) = Id(a)
-      def flatMap[A, B](m: Id[A])(f: A => Id[B]): Id[B] = f(m.value)
+      override def unit[A](a: => A) = Id(a)
+      override def flatMap[A, B](m: Id[A])(f: A => Id[B]): Id[B] = f(m.value)
     }
   }
 }
